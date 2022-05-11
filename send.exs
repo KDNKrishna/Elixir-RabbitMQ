2d8fe88e166ab420f98750add54386f480684668
KDNKrishna <kdnkrishna@gmail.com>
2022-05-11 23:29:40 +0200

    Add files via upload

---
+++ send.exs
@@ -0,0 +1,12 @@
+#This file will send a single message to queue. First thing to establish is a connection with RabbitMQ Server
+
+{:ok, connection} = AMQP.Connection.open
+{:ok, channel} = AMQP.Channel.open(connection)
+
+#Create a queue to which the message will be delivered
+AMQP.Queue.declare(channel, "hello")
+
+AMQP.Basic.publish(channel, "", "hello", "Servicehub")
+IO.puts " [x] Sent 'Servicehub'"
+
+AMQP.Connection.close(connection)
