40ffb5af1e77ec6ad589145a9f92623d50fe249b
KDNKrishna <kdnkrishna@gmail.com>
2022-06-06 22:26:57 +0200

    Update send.exs

--- send.exs
+++ send.exs
@@ -1,6 +1,6 @@
 #This file will send a single message to queue. First thing to establish is a connection with RabbitMQ Server
 
-{:ok, connection} = AMQP.Connection.open
+{:ok, connection} = AMQP.Connection.open("amqp://guest:guest@localhost")
 {:ok, channel} = AMQP.Channel.open(connection)
 
 #Create a queue to which the message will be delivered
